9b7010e2691daa6de0fc833544cb74f95a59baaf
Darvi Sunny <darvi.sunny@tadigital.com>
2023-05-29 19:49:18 +0530

    Rohiths Changes on products

--- src/sxastarter/src/components/Footer.tsx
+++ src/sxastarter/src/components/Footer.tsx
@@ -2,44 +2,54 @@ import { ImageField, Image, withDatasourceCheck } from '@sitecore-jss/sitecore-j
 import { ComponentProps } from 'lib/component-props';
 
 type FooterProps = ComponentProps & {
-    fields: {
-        footerimg: ImageField;
-    };
+  fields: {
+    footerimg: ImageField;
+  };
 };
 const Footer = (props: FooterProps): JSX.Element => {
-
-    return (
-        <div className="footer">
-            <div className="newsletter">
-                <h2>KEEP ME UPDATED</h2>
-                <p>Stay Up To date with the latest Hubolt news</p>
-                <a className="open-button" >Sign Up</a>
-            </div>
-            <div className="footerlogo">
-                <Image field={props.fields.footerimg} />
-            </div>
-            <div className="footernav" >
-                <nav>
-                    <a href="">Newsletter</a>
-                    <a href="">Career</a>
-                    <a href="">Support</a>
-                    <a href="">About Us</a>
-                    <a href="">Hublot Worldwide</a>
-                    <a href="">Terms Of Use</a>
-                    <a href="">Trademarks</a>
-                    <a href="">Accessibility</a>
-                </nav>
-                <nav>
-                    <a href=""><i className="fa fa-facebook"></i></a>
-                    <a href=""><i className="fa fa-instagram"></i></a>
-                    <a href=""><i className="fa fa-twitter"></i></a>
-                    <a href=""><i className="fa fa-github"></i></a>
-                    <a href=""><i className="fa fa-whatsapp"></i></a>
-                    <a href=""><i className="fa fa-youtube"></i></a>
-                </nav>
-
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="footer">
+      <div className="newsletter">
+        <h2>KEEP ME UPDATED</h2>
+        <p>Stay Up To date with the latest Hubolt news</p>
+        <a className="open-button">Sign Up</a>
+      </div>
+      <div className="footerlogo">
+        <Image field={props.fields.footerimg} />
+      </div>
+      <div className="footernav">
+        <nav>
+          <a href="">Newsletter</a>
+          <a href="">Career</a>
+          <a href="">Support</a>
+          <a href="">About Us</a>
+          <a href="">Hublot Worldwide</a>
+          <a href="">Terms Of Use</a>
+          <a href="">Trademarks</a>
+          <a href="">Accessibility</a>
+        </nav>
+        <nav>
+          <a href="">
+            <i className="fa fa-facebook"></i>
+          </a>
+          <a href="">
+            <i className="fa fa-instagram"></i>
+          </a>
+          <a href="">
+            <i className="fa fa-twitter"></i>
+          </a>
+          <a href="">
+            <i className="fa fa-github"></i>
+          </a>
+          <a href="">
+            <i className="fa fa-whatsapp"></i>
+          </a>
+          <a href="">
+            <i className="fa fa-youtube"></i>
+          </a>
+        </nav>
+      </div>
+    </div>
+  );
+};
 export default withDatasourceCheck()<FooterProps>(Footer);
